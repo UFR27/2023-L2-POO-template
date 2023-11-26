07fba388780e07d1bbef711d5b626fb0093334e9
Nicolas Herbaut <nicolas.herbaut@gmail.com>
2023-11-26 15:56:19 +0100

    updated proxy usage

--- src/main/java/fr/pantheonsorbonne/ufr27/miashs/poo/Main.java
+++ src/main/java/fr/pantheonsorbonne/ufr27/miashs/poo/Main.java
@@ -1,5 +1,6 @@
 package fr.pantheonsorbonne.ufr27.miashs.poo;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -9,22 +10,27 @@ import java.util.Objects;
 public class Main {
     public static void main(String ...args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException{
         ItemsScrapper scrapper  = new ItemsScrapper();
-        ArrayList<Item> items = scrapper.parseSource(ContentProxy.getCached());
-        System.out.println("Les Attributs de chaque Item (" + items.size()+")");
-        for(int i=0 ; i<items.size(); i++){
-            System.out.println("Item " + i);
-            for(Field field : Item.class.getDeclaredFields()){
-                field.setAccessible(true);
-                System.out.println(field.getName()+": "+Objects.toString(field.get(items.get(i))));
+        try{
+            ArrayList<Item> items = scrapper.parseSource(ContentProxy.getFresh());
+            System.out.println("Les Attributs de chaque Item (" + items.size()+")");
+            for(int i=0 ; i<items.size(); i++){
+                System.out.println("Item " + i);
+                for(Field field : Item.class.getDeclaredFields()){
+                    field.setAccessible(true);
+                    System.out.println(field.getName()+": "+Objects.toString(field.get(items.get(i))));
+                }
+                System.out.println("------------");
             }
-            System.out.println("------------");
+            System.out.println("=======================");
+            System.out.println("L'analyse des données collectées");
+            ItemAnalyzer itemAnalyzer = new ItemAnalyzer(items);
+            for(Method method : ItemAnalyzer.class.getDeclaredMethods()){
+                System.out.println(method.getName()+ ": " + Objects.toString(method.invoke(itemAnalyzer)));
+            }
         }
-        System.out.println("=======================");
-        System.out.println("L'analyse des données collectées");
-        ItemAnalyzer itemAnalyzer = new ItemAnalyzer(items);
-        for(Method method : ItemAnalyzer.class.getDeclaredMethods()){
-            System.out.println(method.getName()+ ": " + Objects.toString(method.invoke(itemAnalyzer)));
+        catch(IOException e){
+            System.out.println("un problème est survenu lors de la récupération du contenu fresh, utilisez le cache ou réessayez plus tard");
         }
     }
-    
+
 }
